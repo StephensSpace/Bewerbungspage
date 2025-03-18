2f8807151fc6ecb5342eaeaa0e75515a1c3e925f
Stephen Schaz <stephenschaz90@gmail.com>
2025-03-18 01:41:55 +0100

    About Me Started / Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { HereIamComponent } from './here-iam/here-iam.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HereIamComponent],
+  imports: [RouterOutlet, HeaderComponent, HereIamComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
