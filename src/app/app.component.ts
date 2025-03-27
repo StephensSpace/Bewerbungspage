2300a5aca2f762921ed51dc754b323594bddb40c
Stephen Schaz <stephenschaz90@gmail.com>
2025-03-27 01:37:19 +0100

    Portfolio done, going to Comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,15 @@ import { HeaderComponent } from './shared/header/header.component';
 import { HereIamComponent } from './here-iam/here-iam.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CommentsComponent } from './comments/comments.component';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HereIamComponent, AboutMeComponent, PortfolioComponent],
+  imports: [RouterOutlet, HeaderComponent,
+    HereIamComponent, AboutMeComponent,
+    PortfolioComponent, CommentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,20 +1,72 @@
-import { NgFor, NgStyle } from '@angular/common';
+import { NgClass, NgFor, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [NgFor, NgStyle],
+  imports: [NgFor, NgStyle, NgClass],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-  projects: { imgPath: string; link: string; linkGit: string; description: string; reverseRow: boolean }[] = [
-    {
+  projects: {
+    imgPath: string;
+    link: string;
+    linkGit: string;
+    description: string;
+    reverseRow: boolean;
+    name: string;
+    skills: string;
+    commingSoon: boolean
+  }[] = [
+      {
         imgPath: "/assets/projects/Pkedex.svg",
-        link: "",
+        link: "https://pokedex.frontendschaz.de/",
         linkGit: "https://github.com/StephensSpace/Pokedex",
-        description: "",
-        reverseRow: false
-    }
-];
+        description:
+          `Based on the PokéAPI a simple library that provides and catalogues pokemon information.
+           Project design by myself, no professional template.`,
+        reverseRow: false,
+        name: "Pokedex",
+        skills: "JavaScript | HTML | CSS | API",
+        commingSoon: false
+      },
+      {
+        imgPath: "/assets/projects/PolloLoco.svg",
+        link: "https://elpolloloco.frontendschaz.de",
+        linkGit: "https://github.com/StephensSpace/El-Pollo-Loco",
+        description:
+          `A simple Jump-and-Run game based on an object-oriented approach. 
+        Help Pepe to find coins and salsa bottles to fight against the Boss chicken.`,
+        reverseRow: true,
+        name: "El Pollo Loco",
+        skills: "JavaScript | HTML | CSS |  Canvas based ",
+        commingSoon: false
+      },
+      {
+        imgPath: "/assets/projects/Join.svg",
+        link: "https://joinda.frontendschaz.de/welcome.html",
+        linkGit: "https://github.com/StephensSpace/JoinDA",
+        description:
+          `Task manager inspired by the Kanban System. 
+          Create and organize tasks using drag and drop functions, assign users and categories. `,
+        reverseRow: false,
+        name: "Join",
+        skills: "JavaScript | HTML | CSS ",
+        commingSoon: false
+      },
+      {
+        imgPath: "/assets/projects/Dabubble.svg",
+        link: "",
+        linkGit: "",
+        description:
+          `Comming soon, project starts April 2025. This App is a Slack Clone App. 
+          It revolutionizes team communication and collaboration with its intuitive interface,
+           real-time messaging, and robust channel organization.`,
+        reverseRow: true,
+        name: "DA Bubble",
+        skills: "Angular | Typescript | Firebase",
+        commingSoon: true
+      }
+    ];
 }
+
