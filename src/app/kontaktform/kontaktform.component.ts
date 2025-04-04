784fb8da7aa290b8d46f418cdce8b849c9ae42ab
Stephen Schaz <stephenschaz90@gmail.com>
2025-04-04 11:31:59 +0200

    Contactform working php added, livetest necesary

--- src/app/kontaktform/kontaktform.component.ts
+++ src/app/kontaktform/kontaktform.component.ts
@@ -1,31 +1,57 @@
-import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgClass, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-kontaktform',
-  imports: [NgIf, ReactiveFormsModule],
+  imports: [NgIf, FormsModule, NgClass],
   templateUrl: './kontaktform.component.html',
   styleUrl: './kontaktform.component.scss'
 })
 export class KontaktformComponent {
-  contactForm: FormGroup;
+
+  http = inject(HttpClient)
+
   submitted = false;
+  contactData: { name: string; email: string; message: string; privacyPolicy: boolean } = {
+    name: "",
+    email: "",
+    message: "",
+    privacyPolicy: false
+  };
 
-  constructor(private fb: FormBuilder) {
-    this.contactForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      message: ['', [Validators.required, Validators.minLength(10)]]
-    });
-  }
+  mailTest = true;
 
-  sendMail() {
-    this.submitted = true;
-    if (this.contactForm.valid) {
-      const mailtoLink = `mailto:stephenschaz90@gmail.com?subject=Kontaktanfrage&body=${encodeURIComponent(this.contactForm.value.message)}`;
-      window.location.href = mailtoLink;
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  sendMail(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      console.log(ngForm, "des gooot scho")
+      ngForm.resetForm();
     }
   }
+
 }
