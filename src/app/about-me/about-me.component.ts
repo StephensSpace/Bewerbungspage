45f2bb53dba941b59b57ecd0d9c162b3d49fb8b8
Stephen Schaz <stephenschaz90@gmail.com>
2025-04-10 03:36:11 +0200

    language texts in work

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { SkillsComponent } from '../skills/skills.component';
 import { LanguageService } from '../services/language.service';
+import { CommonModule } from '@angular/common';
+import { texts } from '../languageData/languageTexts';
 
 @Component({
   selector: 'app-about-me',
-  imports: [SkillsComponent],
+  imports: [SkillsComponent, CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
+  public texts = texts;
   constructor(public languageService: LanguageService) {}
 }
--- src/app/languageData/languageTexts.ts
+++ src/app/languageData/languageTexts.ts
@@ -1,7 +1,7 @@
 export type TranslatedText = {
     de: string;
     en: string;
-  };
+};
 
 export const texts: {
     header: {
@@ -10,6 +10,8 @@ export const texts: {
     },
     hereIAmBtn: TranslatedText,
     scrollDown: TranslatedText,
+    aboutMeText: TranslatedText,
+    aboutMeLocation: TranslatedText
 
 } = {
     header: {
@@ -21,15 +23,34 @@ export const texts: {
             de: `Kontakt`,
             en: `Contact`
         },
-        
+
     },
     hereIAmBtn: {
-      de: 'Reden wir!',
-      en: `Let's Talk!`,
+        de: 'Reden wir!',
+        en: `Let's Talk!`,
     },
     scrollDown: {
         de: 'Nach unten',
         en: `Scroll down`,
-      },
+    },
+    aboutMeText: {
+        de: `Mein Name ist Stephen Schaz. Seit März 2024 mache ich meine 
+        langjährige Leidenschaft für IT zu meinem Beruf.
+        Mit Unterstützung der Developer Akademie habe ich eine 
+        einjährige Ausbildung zum Junior Software Developer abgeschlossen 
+        – und damit den Grundstein für meinen Weg in die IT-Welt gelegt.`,
+        en: `My name is Stephen Schaz. Since March 2024, I've decided to 
+        make my passion for IT my career. With the support of the Developer 
+        Akademie, I completed a one-year training program as a Junior Software 
+        Developer – laying the foundation for my journey into the world of IT.`
+    },
+    aboutMeLocation: {
+        de: `Ich komme aus dem sonnigen Süden Deutschlands – genauer gesagt aus 
+        Messkirch, einer kleinen Stadt etwa 30 Kilometer vom Bodensee und 100 
+        Kilometer von Stuttgart entfernt, gelegen im sogenannten Badischen Geniewinkel.`,
+        en: `I come from the sunny south of Germany – Messkirch, a small town about 30 
+        kilometers from Lake Constance and 100 kilometers from Stuttgart, located in 
+        what's known as the "Badischer Geniewinkel" (Baden's Genius Region).`
+    }
     
   };
